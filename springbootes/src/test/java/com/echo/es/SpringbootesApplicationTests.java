ccb98fea947de826243f8ce88b7fa0375ec8c04e
echomes <echomes@163.com>
2018-03-26 14:59:23 +0800

    更新

--- springbootes/src/test/java/com/echo/es/SpringbootesApplicationTests.java
+++ springbootes/src/test/java/com/echo/es/SpringbootesApplicationTests.java
@@ -1,7 +1,20 @@
 package com.echo.es;
 
 import java.util.Date;
+import java.util.List;
+import java.util.Map;
 
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.SearchHits;
+import org.elasticsearch.search.aggregations.Aggregation;
+import org.elasticsearch.search.aggregations.AggregationBuilder;
+import org.elasticsearch.search.aggregations.AggregationBuilders;
+import org.elasticsearch.search.aggregations.bucket.terms.Terms;
+import org.elasticsearch.search.aggregations.bucket.terms.Terms.Bucket;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +30,9 @@ public class SpringbootesApplicationTests {
 
 	@Autowired
 	private UserDao userDao;
+	
+	@Autowired
+	private TransportClient client;
 
 	@Test
 	public void contextLoads() {
@@ -33,6 +49,56 @@ public class SpringbootesApplicationTests {
 		u.setId(6l);
 
 		userDao.save(u);
+	}
+
+	@Test
+	public void query() {
+		// Iterable<User> all = userDao.findAll();
+		// for (User user : all) {
+		// System.out.println(user);
+		// }
+		QueryBuilder query = QueryBuilders.boolQuery().
+				must(QueryBuilders.termQuery("name", "测试"))
+				.must(QueryBuilders.rangeQuery("age").from("10").to("30"));
+
+		Iterable<User> iterable = userDao.search(query);
+		System.out.println(query);
+		for (User user : iterable) {
+			System.out.println(user);
+		}
+	}
+	
+	@Test
+	public void agg() {
+		QueryBuilder qr = QueryBuilders.rangeQuery("price").from("10").to("40");
+		
+		AggregationBuilder agg = AggregationBuilders.terms("agg_name").field("name").
+				subAggregation(AggregationBuilders.avg("avg_price").field("price"));
+		
+		SearchResponse response = client.prepareSearch("books").
+				setTypes("book").setQuery(qr).
+				addAggregation(agg).setSize(0).
+				execute().actionGet();
+		
+		System.out.println(response.toString());;
+		SearchHits searchHits = response.getHits();
+		Map<String, Aggregation> asList = response.getAggregations().asMap();
+		
+		for (Map.Entry<String, Aggregation> entity : asList.entrySet()) {
+			Terms terms = (Terms) entity.getValue();
+			List<? extends Bucket> list = terms.getBuckets();
+			
+			for (Bucket b : list) {
+				System.out.println(b.getKey());
+			}
+		}
+		
+		for (SearchHit sh : searchHits) {
+			//System.out.println("id:"+sh.getId()+"索引:"+sh.getIndex()+"Type:"+sh.getType());
+			System.out.println(sh.getSourceAsString());
+		}
+		client.close();
+		
 	}
 
 }
