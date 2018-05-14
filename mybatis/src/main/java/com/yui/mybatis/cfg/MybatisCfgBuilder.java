f0065a0432fe2ee2e224c6ea5178e31b056623a3
echomes <echomes@163.com>
2018-05-14 18:06:56 +0800

    update

---
+++ mybatis/src/main/java/com/yui/mybatis/cfg/MybatisCfgBuilder.java
@@ -0,0 +1,37 @@
+package com.yui.mybatis.cfg;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+/**
+ * 获取sqlsessionFactory
+ * 
+ * @author Echo
+ *
+ */
+public class MybatisCfgBuilder {
+
+	private static String source = "mybatis.cfg.xml";
+
+	private MybatisCfgBuilder() {
+
+	}
+
+	public static SqlSessionFactory sqlSessionFactory() {
+
+		try {
+			InputStream in = Resources.getResourceAsStream(source);
+			SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(in);
+			return sqlSessionFactory;
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+
+	}
+
+}
---
+++ mybatis/src/main/java/com/yui/mybatis/test/MyTest.java
@@ -0,0 +1,76 @@
+package com.yui.mybatis.test;
+
+import java.util.Date;
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+
+import com.google.common.collect.Lists;
+import com.yui.mybatis.cfg.MybatisCfgBuilder;
+import com.yui.mybatis.entity.User;
+import com.yui.mybatis.mapper.UserMapper;
+
+/**
+ * 测试类
+ * 
+ * @author Echo
+ *
+ */
+public class MyTest {
+
+	@Test
+	public void test() {
+		SqlSession session = MybatisCfgBuilder.sqlSessionFactory().openSession();
+		UserMapper userMapper = session.getMapper(UserMapper.class);
+
+		User u = new User();
+		u.setName("%测试%");
+
+		List<User> userList = userMapper.query(u);
+
+		for (User user : userList) {
+			System.out.println(user);
+		}
+
+		session.close();
+	}
+
+	@Test
+	public void testAddList() {
+		SqlSession session = MybatisCfgBuilder.sqlSessionFactory().openSession();
+		UserMapper userMapper = session.getMapper(UserMapper.class);
+
+		List<User> userList = Lists.newArrayList();
+		long start = System.currentTimeMillis();
+		for (int i = 0; i < 10000; i++) {
+			userList.add(new User("测试_" + i, i, new Date()));
+		}
+
+		userMapper.insertList(userList);
+
+		session.commit();
+		session.close();
+		long end = System.currentTimeMillis();
+		System.out.println((end - start) / 1000);
+	}
+	
+	@Test
+	public void testQueryIn() {
+		SqlSession session = MybatisCfgBuilder.sqlSessionFactory().openSession();
+		UserMapper userMapper = session.getMapper(UserMapper.class);
+
+
+		List<String> ids = Lists.newArrayList();
+		//ids.add("1");ids.add("2");
+		
+		
+		List<User> userList = userMapper.queryIn(ids);
+
+		for (User user : userList) {
+			System.out.println(user);
+		}
+
+		session.close();
+	}
+}
